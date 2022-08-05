8fd7c18031848f42f904adb7deb0565a89c8b177
PX001-hub <draivv@yandex.ru>
2022-08-05 22:40:37 +0300

    Initial Commit

---
+++ homeWorkRGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWorkRGB
+//
+//  Created by P Z on 05/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
