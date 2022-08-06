127dd0a970a1825dd324ce47247483c5b091c929
PX001-hub <draivv@yandex.ru>
2022-08-06 17:50:54 +0300

    version 1.0

--- homeWorkRGB/ViewController.swift
+++ homeWorkRGB/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    
+    
 
 }
 
