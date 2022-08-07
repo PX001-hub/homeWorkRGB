8d591f5c660f8bcc275df203d7676bb747ce3edf
PX001-hub <54709568+PX001-hub@users.noreply.github.com>
2022-08-07 22:06:17 +0300

    version 1.2

---
+++ homeWorkRGB/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  homeWorkRGB
+//
+//  Created by P Z on 07/08/2022.
+//
+
+import UIKit
+
+protocol ViewControllerDelegate {
+    func setColor(_ color: UIColor)
+}
+
+class MainViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let colorVC = segue.destination as? ViewController else { return }
+        colorVC.delegate = self
+        colorVC.viewColor = view.backgroundColor
+    }
+}
+
+extension MainViewController: ViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
+
--- homeWorkRGB/ViewController.swift
+++ homeWorkRGB/ViewController.swift
@@ -9,24 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet var colorView: UIView!
     
-    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet weak var greenSlider: UISlider!
     
-    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet var redLable: UILabel!
+    @IBOutlet var greenLable: UILabel!
+    @IBOutlet var blueLable: UILabel!
     
+    
+    var delegate: ViewControllerDelegate!
+    var viewColor: UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        colorView.layer.cornerRadius = 20
+        //colorView.backgroundColor = viewColor
         
-    }
-
-    
-    
-
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        
+//        redLable.delegate = self
+//        greenLable.delegate = self
+//        blueLable.delegate = self
+//
+       // setSliders()
+       
+//   setValue(for: redLabel, greenLabel, blueLabel)
+////
+////    }
+//
+//    @IBAction func rgbSliderAction(_ sender: UISlider) {
+//
+//        switch sender {
+//        case redSlider:
+//            setValue(for: colorView)
+//        case greenSlider:
+//            setValue(for: colorView)
+//        default:
+//            setValue(for: colorView)
+//        }
+//    }
+       
+        setColor()
 }
+}
+
+extension ViewController {
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    
+    }
+    
+//    private func setSliders() {
+//        let ciColor = CIColor(color: viewColor)
+//
+//        redSlider.value = Float(ciColor.red)
+//        greenSlider.value = Float(ciColor.green)
+//        blueSlider.value = Float(ciColor.blue)
+//
+//    }
+
+    
+}
+
+    
 
